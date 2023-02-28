39c6f48f448ec53a4eea96cfdd97cc7bec1560b1
Misa91 <101438118+Misa91@users.noreply.github.com>
2023-02-28 07:35:00 +0000

    [Start]4-25

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "homes#top"
+  get 'homes/about' => 'homes#about', as: 'about'
+  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
+
+end
