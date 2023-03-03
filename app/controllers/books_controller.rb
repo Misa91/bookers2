d2f8ed9429524f46b9bb2466f7c487ceab701dac
Misa91 <101438118+Misa91@users.noreply.github.com>
2023-03-03 08:21:59 +0000

    [Add]Bootstrap使用、バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,22 +8,39 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path #いったん違う画面へリダイレクトさせるが本当はbookの詳細画面にいきたい？
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
-    @user = @book.user #投稿に紐づいたユーザーの画像を表示させたいが、記述が間違っているかも
+    @user = @book.user #投稿に紐づいたユーザーの画像を表示
   end
 
   def edit
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have updated book successfully."
+    else
+      render :edit
+    end
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    #@books = Book.all
     redirect_to '/books'
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,43 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new
     @users = User.all
     @user = current_user
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
 
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+      flash[:notice] = "You have updated user successfully."
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to books_path
+    end
   end
 end
